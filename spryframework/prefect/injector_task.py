6ae4595d6283b93c7b95b48a78ce9d55acf44090
piokra <piotr.krasun@gmail.com>
2021-07-02 01:04:23 +0200

    Added missing module

--- spryframework/prefect/injector_task.py
+++ spryframework/prefect/injector_task.py
@@ -1,9 +1,10 @@
 from injector import Injector
 from prefect import task
 
+from spryframework.influxdb2.influxdb2_module import InfluxDB2ConfigModule, InfluxDB2Module
 from spryframework.vault.vault_module import VaultModule, VaultConfigurationModule
 
 
 @task
 def create_secret_injector_task():
-    return Injector([VaultConfigurationModule(), VaultModule()])
+    return Injector([VaultConfigurationModule(), VaultModule(), InfluxDB2ConfigModule(), InfluxDB2Module()])
